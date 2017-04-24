0e65bdf5ec5e1d087f03ca6e232dceb78cb8e41e
kkkkeeee <zhaikeke1@gmail.com>
2017-04-24 12:50:30 -0700

    added code

---
+++ lb/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#MSUB -q psmall
+#MSUB -l nodes=128
+#MSUB -l partition=vulcan
+#MSUB -l walltime=08:00:00
+#MSUB -V
+#MSUB -l gres=lscratchv
+
+rm -f partxyz*
+rm -f partdata*
+rm -f b3d0.f*
+echo b3d > SESSION.NAME
+echo `pwd`'/' >> SESSION.NAME
+rm -f b3d.sch
+srun -n 128 ./nek5000 > output.txt-n128-lb-32k
+
